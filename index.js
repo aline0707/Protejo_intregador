192cbda9b1f3c81a04e4569e127fe20947ac1717
aline0707 <alinealicate25@gmail.com>
2026-04-08 15:36:19 -0300

    index-atualizado

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const app = require("./app");
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
 
