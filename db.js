7d0d527a31eacd82c20217db3433f1f097071748
aline0707 <alinealicate25@gmail.com>
2026-04-08 15:08:07 -0300

    db-atualizado

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql2");
 
 const conexao = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "sentinelaops"
+  host: "gateway01.us-east-1.prod.aws.tidbcloud.com", // EXEMPLO: Use o host do TiDB
+  user: "seu_usuario.root",                         // Usuário do TiDB
+  password: " ",                    // Senha do TiDB
+  database: "sentinelaops",                         // Nome do banco
+  port: 4000,                                       // O TiDB geralmente usa a porta 4000 (e não 3306)
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true                       // TiDB exige conexão segura SSL
+  }
 });
+
 
 conexao.connect((err) => {
   if (err) {
